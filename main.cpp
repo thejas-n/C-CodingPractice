e02be94897c3c0098df6e50d96452b2d03b3bf88
thejas.n <thejasn61909@gmail.com>
2017-01-12 22:09:38 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char ch;
+   int a;
+   cout<<"enter the char";
+   cin>>ch;
+   a=ch;
+   cout<<"the ASCII value of" << ch <<"is" << a;
+   return 0;
+}
